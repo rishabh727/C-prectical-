f26eab11cf9cf1253d24300cfc77c4e90326bbdd
rishabh727 <143151167+rishabh727@users.noreply.github.com>
2024-05-14 13:18:16 +0530

    Update and rename 12_File Content Copy with Whitespaces Removal.cpo to 12_File Content Copy with Whitespaces Removal.cpp

---
+++ 12_File Content Copy with Whitespaces Removal.cpp
@@ -0,0 +1,25 @@
+// Copy the contents of one text file to another file, after removing all whitespaces.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // first file is student.txt
+    // will be created in runtime named as clean.txt
+    ifstream file;
+    file.open("file1.txt", ios::in);
+    ofstream file2;
+    file2.open("file2.txt", ios::out);
+    char ch; // to read each character from file
+    while (file.get(ch)) {
+        if (ch != ' ' && ch != '\n') {
+            file2.put(ch);
+        }
+    }
+    file.close();
+    file2.close();
+
+    return 0;
+}
